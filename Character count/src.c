7192900a6ff576453e243b677deab37fcac0ad5e
Lonecannibal <aakashsd2000@gmail.com>
2021-09-28 09:52:02 +0530

    Added vowel and consonant count

--- Character count/src.c
+++ Character count/src.c
@@ -1,33 +1,51 @@
 //Read the number of characters, numbers, special characters, etc from a file
 #include <stdio.h>
 #include <stdbool.h>
-char special[]="<>,./?'|\"[]{})(*&^%$#@!-_=+)";
-char types[6][30]={"Upper case", "Lower case", "Numbers", "Special characters", "Spaces","Total characters"};
+#include <string.h>
+char special[]="<>,.;/?'|\"[]{})(*&^%$#@!-_=+)";
+char vowels[]="AEIOUaeiou";
+char types[8][30]={"Upper case", "Lower case", "Numbers", "Special characters", "Spaces","Vowels", "Consonants","Total characters"};
 bool isSpecialChar(char ch){
     for(int i=0;i<strlen(special);i++)
         if(special[i]==ch)
             return true;
     return false;
 }
+bool isVowel(char ch){
+     for(int i=0;i<strlen(vowels);i++)
+        if(vowels[i]==ch)
+            return true;
+    return false;
+}
 void main(){
     FILE *ptr;
     char ch;
-    int count[6]={0,0,0,0,0,0};
+    int count[8]={0,0,0,0,0,0,0,0};
     ptr=fopen("input.txt","r");
     while(fscanf(ptr,"%c",&ch)!=EOF){
-        if(ch>='A' && ch<='Z')
+        if(ch>='A' && ch<='Z'){
             count[0]++;
-        else if((ch>='a' && ch<='z'))
+            if(isVowel(ch))
+                count[5]++;
+            else
+                count[6]++;
+        }
+        else if((ch>='a' && ch<='z')){
             count[1]++;
+            if(isVowel(ch))
+                count[5]++;
+            else
+                count[6]++;
+        }
         else if(ch>=48 && ch<=57)
             count[2]++;
         else if(isSpecialChar(ch))
             count[3]++;
         else if(ch==' ')
             count[4]++;
-        count[5]++;
+        count[7]++;
     }
-    for(int i=0;i<6;i++)
+    for(int i=0;i<8;i++)
         printf("%s: %d\n",types[i],count[i]);
     fclose(ptr);
 }
