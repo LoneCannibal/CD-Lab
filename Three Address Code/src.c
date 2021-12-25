dcfc3e1f654cf3630630535996c37202c769c8a1
Lonecannibal <aakashsd2000@gmail.com>
2021-12-25 18:14:25 +0530

    Generated codes

--- Three Address Code/src.c
+++ Three Address Code/src.c
@@ -2,11 +2,17 @@
 #include<stdio.h>
 #include<string.h>
 char input[100];
+int codes[50][3], codeCount=0;
+
 int main(){
     printf("Enter the  input string: ");//Eg. z=a+b*c
     scanf("%s",input);
-    for(int i=0;i<strlen(input);i++)
-        if(!isalpha(input[i]))//If the character is a symbol like /*+- send it's index location to the next function.
-            operator_priority(i);
+    for(int i = 0; i < strlen(input); i++)
+        if(!isalpha(input[i])){//If the character is a symbol like /*+- 
+            sprintf(codes[codeCount],"%c%c%c",input[i-1],input[i],input[i+1]);
+            codeCount++;
+        }
+    for(int i = 0; i <codeCount; i++)
+        printf("%s\n",codes[i]);
     return 0;
 }
